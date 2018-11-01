82667639f3f2b1f8e0ac550f727c7a1a21aaef45
Luis Yucra <luis.yucra@gmail.com>
2018-11-01 08:55:49 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Usage
+
+You should configure to be used the Atlas SDK Maven installation. The test that I do were done with Intellij
+
+ ![](Screenshot_1.png)
+ 
+ 
---
+++ src/main/java/com/milkneko/atlassianaddonjunittesttemplate/AtlassianAddonJunitTestTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.milkneko.atlassianaddonjunittesttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlassianAddonJunitTestTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtlassianAddonJunitTestTemplateApplication.class, args);
+    }
+}
